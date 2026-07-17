0849c0383057ec0263d2b70b6cca7f4e0d14ebbf
tridha <maneeshakollu4@gmail.com>
2026-07-17 12:15:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,161 @@
+// Task 1 : Employee Registration
+const companyName = "Stackly";
+let employeeName = prompt("Maneesha:");
+let employeeID = "STK-26-3900";
+
+console.log("Company : " + companyName);
+console.log("Employee Name : " + employeeName);
+console.log("Employee ID : " + employeeID);
+
+// Task 2 : College Admission
+
+const collegeName = "JNTU";
+let studentName = prompt("Enter Student Name:");
+let course = prompt("ECE:");
+
+console.log("College : " + collegeName);
+console.log("Student : " + studentName);
+console.log("Course : " + course);
+
+// Task 3 : Food Order
+
+const restaurantName = "Food Park";
+let customerName = prompt("Customer Name:");
+let foodName = prompt("Food Name:");
+let quantity = prompt("Quantity:");
+
+console.log("Restaurant : " + restaurantName);
+console.log("Customer : " + customerName);
+console.log("Food : " + foodName);
+console.log("Quantity : " + quantity);
+
+// Task 4 : Hospital Registration
+
+const hospitalName = "Apollo";
+let patientName = prompt("Patient Name:");
+let tokenNumber = prompt("Token Number:");
+
+console.log("Hospital : " + hospitalName);
+console.log("Patient : " + patientName);
+console.log("Token : " + tokenNumber);
+
+// Task 5 : Movie Ticket Booking
+
+const theatreName = "PVR Cinemas";
+let movieName = prompt("Movie Name:");
+let movieCustomer = prompt("Customer Name:");
+let seatNumber = prompt("Seat Number:");
+
+console.log("Theatre : " + theatreName);
+console.log("Movie : " + movieName);
+console.log("Customer : " + movieCustomer);
+console.log("Seat Number : " + seatNumber);
+
+// Task 6 : Mobile Shop
+
+const shopName = "Sangeetha Mobiles";
+let mobileCustomer = prompt("Customer Name:");
+let mobileModel = prompt("Mobile Model:");
+let price = prompt("Mobile Price:");
+
+console.log("Shop : " + shopName);
+console.log("Customer : " + mobileCustomer);
+console.log("Mobile Model : " + mobileModel);
+console.log("Price : " + price);
+
+// Task 7 : Gym Membership
+
+const gymName = "Fitness World";
+let memberName = prompt("Member Name:");
+let membershipFee = prompt("Membership Fee:");
+
+console.log("Gym : " + gymName);
+console.log("Member : " + memberName);
+console.log("Membership Fee : " + membershipFee);
+
+// Task 8 : ATM Welcome Screen
+
+const bankName = "SBI";
+let userName = prompt("User Name:");
+
+console.log("Welcome " + userName);
+console.log("Bank : " + bankName);
+
+// Task 9 : Bus Ticket
+
+const travelsName = "Orange Travels";
+let passengerName = prompt("Passenger Name:");
+let busSeat = prompt("Seat Number:");
+
+
+console.log("Travels : " + travelsName);
+console.log("Passenger : " + passengerName);
+console.log("Seat Number : " + busSeat);
+
+// Task 10 : Online Shopping
+
+const websiteName = "Amazon";
+let productName = prompt("Product Name:");
+let shoppingCustomer = prompt("Customer Name:");
+let productPrice = prompt("Product Price:");
+
+console.log("Website : " + websiteName);
+console.log("Product : " + productName);
+console.log("Customer : " + shoppingCustomer);
+console.log("Price : " + Price);
+
+// Task 11 : console.log()
+
+console.log("Registration Successful");
+
+// Task 12 : console.error()
+
+console.error("Payment Failed");
+
+// Task 13 : console.warn()
+
+console.warn("Battery Low");
+
+// Task 14 : console.clear()
+
+console.log("Loading...");
+console.log("Welcome");
+console.clear();
+
+// Task 15 : alert()
+
+alert("Welcome to Amazon");
+
+// Task 16 : confirm()
+
+let logout = confirm("Do you want to logout?");
+console.log(logout);
+
+// Task 17 : prompt()
+
+let city = prompt(" Your City:");
+console.log("Your City : " + city  );
+
+// Task 18 : var Practice
+
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+
+console.log("Product : " + product);
+
+
+
+// Task 19 : let Practice
+
+let salary = 25000;
+salary = 30000;
+
+console.log("Salary : " + salary);
+
+
+// Task 20 : const Practice
+
+const company = "Google";
+
+console.log("Company : " + company);
